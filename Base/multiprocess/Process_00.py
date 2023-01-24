7f149f6de6b9104ce324c95b2178d9941f2d4816
digalg <995966061@qq.com>
2023-01-24 04:09:51 -0500

    modify 'coding : utf-8'-->'coding:utf-8'

--- Base/multiprocess/Process_00.py
+++ Base/multiprocess/Process_00.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# -*- coding : utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import os, sys
 from multiprocessing import Process
--- spider/doubanmv_v0.2.py
+++ spider/doubanmv_v0.2.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# -*- coding : utf-8 -*-
+# -*- coding:utf-8 -*-
 
 ###invoke multiprocessing
 
